a9d0ebdcde6e7ec76edaabfacd9a79e797595e75
dzoric1 <Gerasimov2099@mail.ru>
2023-10-26 23:17:38 +0800

    realized popup and adapted

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './index.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const openPopupBtn = document.querySelector('.header__nav-button');
+  const popup = document.querySelector('.popup');
+  const popupCloseBtn = document.querySelector('.popup__close-btn');
+
+  openPopupBtn.addEventListener('mousedown', () => {
+    popup.classList.add('popup_opened');
+  });
+
+  popupCloseBtn.addEventListener('mousedown', () => {
+    popup.classList.remove('popup_opened');
+  });
+
+});
