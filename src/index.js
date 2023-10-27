6695c09bf63357664d66c7a022b918ac98a6402e
dzoric1 <Gerasimov2099@mail.ru>
2023-10-27 12:17:33 +0800

    realized animations

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const openPopupBtn = document.querySelector('.header__nav-button');
   const popup = document.querySelector('.popup');
   const popupCloseBtn = document.querySelector('.popup__close-btn');
+  const background = document.querySelector('.background');
 
   openPopupBtn.addEventListener('mousedown', () => {
     popup.classList.add('popup_opened');
@@ -13,4 +14,10 @@ document.addEventListener('DOMContentLoaded', () => {
     popup.classList.remove('popup_opened');
   });
 
+  const paralax = (e) => {
+    let x = e.clientX / window.innerWidth;
+    background.style.transform = `translate(-${x * 100}px)`
+  }
+
+  document.addEventListener('mousemove', paralax)
 });
